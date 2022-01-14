4dcf8dcd4cd567761d2f079b707652635a00d29a
Francesca Obordo <fobordo@gmail.com>
2022-01-14 11:08:20 -0500

    Sorted the cities and filtered to the top five cities by pop. growth.

--- plot.js
+++ plot.js
@@ -1,1 +1,10 @@
+// Display cityGrowths
 console.log(cityGrowths);
+
+// Sort the cities by population growth
+var sortedCities = cityGrowths.sort((a,b) => a.Increase_from_2016 - b.Increase_from_2016).reverse();
+console.log(sortedCities);
+
+// Select only the top five cities by population growth
+var topFiveCities = sortedCities.slice(0,5);
+console.log(topFiveCities);
